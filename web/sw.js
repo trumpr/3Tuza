81c28b4a4187ea63e51b5181e3542f6843eeb1b0
trumpr <sizin_email@example.com>
2026-08-24 22:34:34 +0400

    Fix APK download by excluding from SW

--- web/sw.js
+++ web/sw.js
@@ -7,5 +7,9 @@ self.addEventListener('activate', (e) => {
 });
 
 self.addEventListener('fetch', (e) => {
+  // APK fayllarını Service Worker-dən kənar tut (Yükləmə xətasını düzəltmək üçün)
+  if (e.request.url.endsWith('.apk')) {
+    return;
+  }
   e.respondWith(fetch(e.request));
 });
